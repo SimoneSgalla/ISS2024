1fddaa11497ab3957a6aa6a6f948df8a3feacd49
S1098357 <s1098357@studenti.univpm.it>
2024-03-11 15:10:39 +0100

    implemento obslogger

--- ProdCons0/src/unibo/naiveactors24/example/MainExampleNaiveActors24.java
+++ ProdCons0/src/unibo/naiveactors24/example/MainExampleNaiveActors24.java
@@ -1,4 +1,7 @@
 package unibo.naiveactors24.example;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import unibo.basicomm23.msg.ProtocolType;
 import unibo.basicomm23.utils.CommUtils;
 import unibo.naiveactors24.ActorContext24;
@@ -12,7 +15,7 @@ public class MainExampleNaiveActors24 {
 		AlienJava caller = new AlienJava("javacaller", ProtocolType.tcp, "localhost", "8123");
 		caller.activate();
 	}
-    public void configureTheSystem(){
+    public void configureTheSystem() throws IOException{
          
         int port1 = 8123;
         CommUtils.outblue("MainExampleTowardsActors24 CREA I CONTESTI ");
@@ -21,10 +24,11 @@ public class MainExampleNaiveActors24 {
 
         Actor24Sender a1   = new Actor24Sender("produttore",ctx1);
         Actor24Receiver a2 = new Actor24Receiver("consumatore",ctx1);
-        obslogger o= new obslogger("obslogger", ctx1);
+        obslogger o = new obslogger("obslogger", ctx1);
 
         ctx1.showActorNames();
         
+        o.activateAndStart();
         a1.activateAndStart();
         a2.activateAndStart();
         
@@ -33,10 +37,11 @@ public class MainExampleNaiveActors24 {
         CommUtils.outblue("PROVO UPDATE ");
         a1.addObserver(o.getName());
         a2.addObserver(o.getName());
-        a1.updateResources("bo");
+        a1.updateResources("prova");
         a2.updateResources("prova1");
+        
     }
-    public static void main(String[] args ){
+    public static void main(String[] args ) throws IOException{
         new MainExampleNaiveActors24().configureTheSystem();
     }
 }
--- ProdCons0/src/unibo/naiveactors24/obslogger.java
+++ ProdCons0/src/unibo/naiveactors24/obslogger.java
@@ -1,7 +1,10 @@
 package unibo.naiveactors24;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 import unibo.basicomm23.interfaces.IApplMessage;
 import unibo.basicomm23.msg.ProtocolType;
@@ -9,18 +12,18 @@ import unibo.basicomm23.utils.CommUtils;
 
 public class obslogger extends ActorBasic24{
 		
-	public obslogger(String name, ActorContext24 ctx) {
+	public obslogger(String name, ActorContext24 ctx) throws IOException {
         super(name,ctx);
-        new File("obsloggerLog.txt");
     }
 	
-	protected void elabMsg(IApplMessage msg) throws Exception {
+	protected void elabMsg(IApplMessage msg) throws IOException {
         CommUtils.outgreen(name + " | elab " + msg + " in:" + Thread.currentThread().getName());
-        if( msg.isDispatch() ) {
-        	FileWriter myWriter = new FileWriter("filename.txt");
-        	myWriter.write("msg");
-        	myWriter.close();
+        if( msg.msgId()=="update" ) {
+        	FileWriter fstream = new FileWriter("obslogger.txt",true);
+        	BufferedWriter out = new BufferedWriter(fstream);
+        	CommUtils.outblue("SCRIVO FILE ");
+        	out.write(msg.toString()+"\n");
+        	out.close();
         }
 	}
-
 }
